cad3ffa445be7fe68ec039121240d2a8a19ddce5
nilshenrich <nilshenrich@web.de>
2022-04-15 15:18:08 +0200

    Completed tets to start TCP server

--- gtest/src/TcpServer_Test_Start.cpp
+++ gtest/src/TcpServer_Test_Start.cpp
@@ -20,7 +20,32 @@ void TcpServer_Test_Start::TearDown()
     return;
 }
 
+// ====================================================================================================================
+// Desc:       Check if the TCP server starts properly
+// Steps:      Start TCP server with correct parameters
+// Exp Result: NETWORKLISTENER_START_OK
+// ====================================================================================================================
 TEST_F(TcpServer_Test_Start, PosTest)
 {
     EXPECT_EQ(tcpServer.start(port), NETWORKLISTENER_START_OK);
+}
+
+// ====================================================================================================================
+// Desc:       Check if server doesn't accept negative port number
+// Steps:      Try to start TCP server with -1
+// Exp Result: NETWORKLISTENER_ERROR_START_WRONG_PORT
+// ====================================================================================================================
+TEST_F(TcpServer_Test_Start, NegTest_WrongPort_Negative)
+{
+    EXPECT_EQ(tcpServer.start(-1), NETWORKLISTENER_ERROR_START_WRONG_PORT);
+}
+
+// ====================================================================================================================
+// Desc:       Check if server doesn't accept too big port number
+// Steps:      Try to start TCP server with 65536
+// Exp Result: NETWORKLISTENER_ERROR_START_WRONG_PORT
+// ====================================================================================================================
+TEST_F(TcpServer_Test_Start, NegTest_WrongPort_TooBig)
+{
+    EXPECT_EQ(tcpServer.start(65536), NETWORKLISTENER_ERROR_START_WRONG_PORT);
 }
