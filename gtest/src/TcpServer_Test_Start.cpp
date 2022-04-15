fa349ac950bcac0fc44624c00e33416e9389552f
nilshenrich <nilshenrich@web.de>
2022-04-15 15:24:44 +0200

    Now TCP server start tests are completed

--- gtest/src/TcpServer_Test_Start.cpp
+++ gtest/src/TcpServer_Test_Start.cpp
@@ -49,3 +49,26 @@ TEST_F(TcpServer_Test_Start, NegTest_WrongPort_TooBig)
 {
     EXPECT_EQ(tcpServer.start(65536), NETWORKLISTENER_ERROR_START_WRONG_PORT);
 }
+
+// ====================================================================================================================
+// Desc:       Check if server doesn't start if it is already running
+// Steps:      Start TCP server and then try to start it again
+// Exp Result: -1
+// ====================================================================================================================
+TEST_F(TcpServer_Test_Start, NegTest_AlreadyRunning)
+{
+    EXPECT_EQ(tcpServer.start(port), NETWORKLISTENER_START_OK);
+    EXPECT_EQ(tcpServer.start(port), -1);
+}
+
+// ====================================================================================================================
+// Desc:       Check if server doesn't start if port is already in use
+// Steps:      Start TCP server and try starting another instance with same port
+// Exp Result: NETWORKLISTENER_ERROR_START_BIND_PORT
+// ====================================================================================================================
+TEST_F(TcpServer_Test_Start, NegTest_PortInUse)
+{
+    ASSERT_EQ(tcpServer.start(port), NETWORKLISTENER_START_OK);
+    TestApi::TcpServerApi tcpServer2{};
+    EXPECT_EQ(tcpServer2.start(port), NETWORKLISTENER_ERROR_START_BIND_PORT);
+}
