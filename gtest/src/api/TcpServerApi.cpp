ba2c32f0039f38aaf33bef58f1024402aae4a5fb
nilshenrich <nilshenrich@web.de>
2022-04-15 11:41:02 +0200

    Created API for TCP server

---
+++ gtest/src/api/TcpServerApi.cpp
@@ -0,0 +1,49 @@
+#include "TcpServerApi.h"
+
+using namespace std;
+using namespace TestApi;
+using namespace networking;
+
+TcpServerApi::TcpServerApi() : TcpServer{'\x00'} {}
+TcpServerApi::~TcpServerApi() {}
+
+int TcpServerApi::start(const int port)
+{
+    return TcpServer::start(port);
+}
+
+void TcpServerApi::stop()
+{
+    TcpServer::stop();
+    return;
+}
+
+bool TcpServerApi::sendMsg(const int tcpClientId, const std::string &tcpMsg)
+{
+    return TcpServer::sendMsg(tcpClientId, tcpMsg);
+}
+
+vector<MessageFromClient> TcpServerApi::getBufferedMsg()
+{
+    return move(bufferedMsg);
+}
+
+vector<int> TcpServerApi::getClientIds()
+{
+    // Get IDs from activeConnections
+    vector<int> clientIds;
+    for (auto &client : activeConnections)
+        clientIds.push_back(client.first);
+    return clientIds;
+}
+
+void TcpServerApi::workOnMessage_TcpServer(const int tcpClientId, const std::string tcpMsgFromClient)
+{
+    bufferedMsg.push_back({tcpClientId, move(tcpMsgFromClient)});
+    return;
+}
+
+void TcpServerApi::workOnClosed_TcpServer(const int tcpClientId)
+{
+    return;
+}
