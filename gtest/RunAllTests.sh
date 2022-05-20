552b9be2794207f69e2e097a9b4615b74794fed5
nilshenrich <nilshenrich@web.de>
2022-05-20 09:58:10 +0200

    Added script to automatically start run all tests and safe result

---
+++ gtest/RunAllTests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get directory of this script
+currentDir=$(dirname $(readlink -f $0))
+
+# Run all tests and safe the results in a file
+cd $currentDir/build/
+./networktester --gtest_output=xml:$currentDir/results.xml
