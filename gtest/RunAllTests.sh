32e9ddfcc380a97279c4a96da2af05b4aa925354
nilshenrich <nilshenrich@web.de>
2022-11-27 14:43:10 +0100

    Fixed Issue #5

--- gtest/RunAllTests.sh
+++ gtest/RunAllTests.sh
@@ -5,4 +5,6 @@ currentDir=$(dirname $(readlink -f $0))
 
 # Run all tests and safe the results in a file
 cd $currentDir/build/
-./networktester --gtest_output=json:$currentDir/results.json
+./networktester --gtest_filter=Forwarding_* --gtest_output=json:$currentDir/results_forwarding.json
+./networktester --gtest_filter=Fragmentation_* --gtest_output=json:$currentDir/results_Fragmentation.json
+./networktester --gtest_filter=General_* --gtest_output=json:$currentDir/results_general.json
