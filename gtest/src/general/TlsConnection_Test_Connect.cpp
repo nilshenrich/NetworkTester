6305c0b76508d473cc78ddc3d23eae67f3f0b1b5
nilshenrich <nilshenrich@web.de>
2022-12-07 14:37:07 +0100

    As the server doesn't confirm established connection, the client can't know in start() if server accepted the connection

--- gtest/src/general/TlsConnection_Test_Connect.cpp
+++ gtest/src/general/TlsConnection_Test_Connect.cpp
@@ -53,37 +53,37 @@ TEST_F(General_TlsConnection_Test_Connect, PosTest_SecondCerts)
 
 // ====================================================================================================================
 // Desc:       Check if the TLS server rejects connections with self signed certificates
-// Steps:      Try to connect to server a with self signed certificate
-// Exp Result: NETWORKLISTENER_START_OK, NETWORKCLIENT_ERROR_START_CONNECT_INIT
+// Steps:      Connect to server a with self signed certificate
+// Exp Result: NETWORKLISTENER_START_OK, NETWORKCLIENT_START_OK
 // ====================================================================================================================
 TEST_F(General_TlsConnection_Test_Connect, NegTest_ClientWithSelfSignedCert)
 {
     EXPECT_EQ(tlsServer.start(port, KeyPaths::CaCert, KeyPaths::ListenerCert, KeyPaths::ListenerKey), NETWORKLISTENER_START_OK);
-    EXPECT_EQ(tlsClient.start("localhost", port, KeyPaths::CaCert, SelfSignedKeyPaths::ClientCert, SelfSignedKeyPaths::ClientKey), NETWORKCLIENT_ERROR_START_CONNECT_INIT);
+    EXPECT_EQ(tlsClient.start("localhost", port, KeyPaths::CaCert, SelfSignedKeyPaths::ClientCert, SelfSignedKeyPaths::ClientKey), NETWORKCLIENT_START_OK);
     EXPECT_EQ(tlsServer.getClientIds().size(), 0);
 }
 
 // ====================================================================================================================
 // Desc:       Check if the TLS server rejects connections with certificates signed by unknown CA
-// Steps:      Try to connect to server a with certificates signed by unknown CA
-// Exp Result: NETWORKLISTENER_START_OK, NETWORKCLIENT_ERROR_START_CONNECT_INIT
+// Steps:      Connect to server a with certificates signed by unknown CA
+// Exp Result: NETWORKLISTENER_START_OK, NETWORKCLIENT_START_OK
 // ====================================================================================================================
 TEST_F(General_TlsConnection_Test_Connect, NegTest_ClientWithUnknownCa)
 {
     EXPECT_EQ(tlsServer.start(port, KeyPaths::CaCert, KeyPaths::ListenerCert, KeyPaths::ListenerKey), NETWORKLISTENER_START_OK);
-    EXPECT_EQ(tlsClient.start("localhost", port, KeyPaths::CaCert, SecondKeyPaths::ClientCert, SecondKeyPaths::ClientKey), NETWORKCLIENT_ERROR_START_CONNECT_INIT);
+    EXPECT_EQ(tlsClient.start("localhost", port, KeyPaths::CaCert, SecondKeyPaths::ClientCert, SecondKeyPaths::ClientKey), NETWORKCLIENT_START_OK);
     EXPECT_EQ(tlsServer.getClientIds().size(), 0);
 }
 
 // ====================================================================================================================
 // Desc:       Check if the TLS server rejects connections with certificates with a certificate chain depth of 2
-// Steps:      Try to connect to server a with certificates with a certificate chain depth of 2
-// Exp Result: NETWORKLISTENER_START_OK, NETWORKCLIENT_ERROR_START_CONNECT_INIT
+// Steps:      Connect to server a with certificates with a certificate chain depth of 2
+// Exp Result: NETWORKLISTENER_START_OK, NETWORKCLIENT_START_OK
 // ====================================================================================================================
 TEST_F(General_TlsConnection_Test_Connect, NegTest_ClientWithCertChainDepth2)
 {
     EXPECT_EQ(tlsServer.start(port, KeyPaths::CaCert, KeyPaths::ListenerCert, KeyPaths::ListenerKey), NETWORKLISTENER_START_OK);
-    EXPECT_EQ(tlsClient.start("localhost", port, KeyPaths::CaCert, CertChainDepth2KeyPaths::ClientCert, CertChainDepth2KeyPaths::ClientKey), NETWORKCLIENT_ERROR_START_CONNECT_INIT);
+    EXPECT_EQ(tlsClient.start("localhost", port, KeyPaths::CaCert, CertChainDepth2KeyPaths::ClientCert, CertChainDepth2KeyPaths::ClientKey), NETWORKCLIENT_START_OK);
     EXPECT_EQ(tlsServer.getClientIds().size(), 0);
 }
 
